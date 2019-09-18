20b9a664dec308b4a664f79e652e949910fee4d0
Micheal Waldrup <waldrupm@gmail.com>
2019-09-17 20:50:11 -0400

    Add middleware, use REST client for post routes

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+// MIDDLEWARE
+app.use(bodyParser.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+// MOCK DATA
 let notes = [
   {
     id: 1,
@@ -22,6 +37,15 @@ let notes = [
   }
 ]
 
+//HELPERS
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+// ROUTES
 app.get('/', (req, res) => {
   res.send('<h1>Hi there</h1>')
 })
@@ -46,6 +70,33 @@ app.delete('/notes/:id', (req, res) => {
 
   res.status(204).end()
 })
+
+app.post('/notes', (req, res) => {
+  const body = req.body
+  
+  if(!body.content) {
+    return res.status(400).json({
+      error: 'Content Missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  res.json(note)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
