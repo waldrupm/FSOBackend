58874bfdcbd5b6b9dfff9557f31f30f154d3e02b
Micheal Waldrup <waldrupm@gmail.com>
2019-09-18 09:16:19 -0400

    Add cors, setup for heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // MIDDLEWARE
 app.use(bodyParser.json())
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -98,7 +100,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log('Server running on port ', PORT)
 })
