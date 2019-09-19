d5a6b73d566cb60c657b20334b5be7d2e4bbb3ca
Micheal Waldrup <waldrupm@gmail.com>
2019-09-19 12:41:22 -0400

    Create note model, start updating routes

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Note = require('./models/note')
+
 
 // MIDDLEWARE
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('build'))
-
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -17,28 +19,6 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
-
-// MOCK DATA
-let notes = [
-  {
-    id: 1,
-    content: "HTML is totally easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
 
 //HELPERS
 const generateId = () => {
@@ -54,17 +34,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes.map(note => note.toJSON()))
+  })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  Note.findById(req.params.id).then(note => {
+    res.json(note.toJSON())
+  })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -77,22 +55,19 @@ app.delete('/api/notes/:id', (req, res) => {
 app.post('/api/notes', (req, res) => {
   const body = req.body
   
-  if(!body.content) {
-    return res.status(400).json({
-      error: 'Content Missing'
-    })
+  if (body.content === undefined) {
+    return res.status(400).json({error: 'Content missing'})
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId()
-  }
+    date: new Date()
+  })
 
-  notes = notes.concat(note)
-
-  res.json(note)
+  note.save().then(savedNote => {
+    res.json(savedNote.toJSON())
+  })
 })
 
 const unknownEndpoint = (request, response) => {
