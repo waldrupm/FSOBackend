9a56acd6bdd30e41addf07f56764b942caec8259
Micheal Waldrup <waldrupm@gmail.com>
2019-09-26 11:04:34 -0400

    Add tests to safely refactor to async-await

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,14 +1,66 @@
-const mongoose = require('mongoose')
 const supertest = require('supertest')
+const mongoose = require('mongoose')
 const app = require('../app')
-
+const helper = require('./test_helper')
 const api = supertest(app)
+const Note = require('../models/note')
 
-test('Notes are returned as JSON', async () => {
+beforeEach(async () => {
+  await Note.deleteMany({})
+
+  let noteObject = new Note(helper.initialNotes[0])
+  await noteObject.save()
+
+  noteObject = new Note(helper.initialNotes[1])
+  await noteObject.save()
+})
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body.length).toBe(helper.initialNotes.length)
+})
+
+test('a specific note is within the returned notes', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(n => n.content)
+
+  expect(contents).toContain('Browser can execute only Javascript')
+})
+
+test('a valid note can be added ', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true
+  }
+
   await api
-    .get('/api/notes')
+    .post('/api/notes')
+    .send(newNote)
     .expect(200)
     .expect('Content-Type', /application\/json/)
+
+  const notesAtEnd = await helper.notesInDb()
+  expect(notesAtEnd.length).toBe(helper.initialNotes.length + 1)
+
+  const contents = notesAtEnd.map(n => n.content)
+  expect(contents).toContain('async/await simplifies making async calls')
+})
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  expect(notesAtEnd.length).toBe(helper.initialNotes.length)
 })
 
 afterAll(() => {
